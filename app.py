d743c8ced3a53c9e54125efb9e81b5fb71f02210
Alex Mendoza <alexandermen2017@fau.edu>
2025-12-28 16:44:39 -0500

    fixing the need to put password every commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 from flask import Flask, render_template 
 
 app = Flask (__name__)
-#testing local connection
+#fixing the need to put password's on Github
 
 def fetch_espn_games():
 	url = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard"
