376b958cff35da751bf369cfdd67d56032dabddb
Alexander Mendoza <alexandermen2017@fau.edu>
2026-01-03 16:47:39 -0500

    change port # for mac

--- app.py
+++ app.py
@@ -104,4 +104,4 @@ def nba():
 	return render_template('index.html', games=games, league = "NBA")
 
 if __name__ == "__main__":
- 	app.run(host = "0.0.0.0", port = 5000)
+ 	app.run(host = "0.0.0.0", port = 5050)
